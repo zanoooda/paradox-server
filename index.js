fb7a789d2fd509396c841ec90aa1dd0a5919ed62
zanoooda <zanooda@ya.ru>
2017-07-31 12:53:48 +0300

    try connection

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 var http = require('http');
-
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello World\n");
+    res.end("Paradox server\n");
 });
 
 server.listen(process.env.PORT);
-
 var io = require('socket.io')(server);
 
 io.on('connection', function (socket) {
-  console.log('server listen');
 
-  socket.on('event', function (data) {
-      console.log(JSON.stringify(data));
-  });
-  //
+    console.log('who connected');
+
+    socket.on('play', function (data) {
+        //
+        console.log('who want to play, looking for the opponent');
+        var data = { opponent: 'petya'};
+
+        socket.emit('get-opponent', data);
+    });
 });
-
-console.log(process.env.PORT);
