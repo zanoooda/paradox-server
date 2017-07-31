1ff2d441b6f4797bd79d8fd3f517b356847a0cc3
zanoooda <zanooda@ya.ru>
2017-07-31 17:12:01 +0300

    16

--- index.js
+++ index.js
@@ -11,19 +11,6 @@ io.on('connection', function (socket) {
 
     console.log(socket.id.toString() + ' connected');
 
-    //var clients = io.sockets.clients();
-    // clients.sockets.forEach(function(element) {
-    //     console.log(element.id);
-    // }, this);
-    // console.log(clients);
-
-    var ids = Object.keys(io.sockets.connected);
-    ids.forEach(function(id) {
-        var s = io.sockets.connected[id];
-        // do something with socket here
-        console.log(s.id, s.isOnline, s.isPlaying);
-    });
-
     socket.on('play', function (data) {
         console.log(socket.id.toString() + ' want to play, looking for the opponent');
 
@@ -31,8 +18,27 @@ io.on('connection', function (socket) {
         socket.isPlaying = false;
 
         //
+        var b = false;
+        var ids = Object.keys(io.sockets.connected);
+        ids.forEach(function(id) {
+            var s = io.sockets.connected[id];
+            // do something with socket here
+            if(!b && s.isOnline && !s.isPlaying && s.id != socket.id) {
+                s.isPlaying = true;
+                socket.isPlaying = true;
+                s.opponentId = socket.id;
+                socket.opponentId = s.id;
+
+                b = true;
+
+                socket.emit('get-opponent', s.id);
+            }
+            //console.log(s.id, s.isOnline, s.isPlaying);
+        });
+
+        if(!b) {
+            // no opponents right now
+        }
         
-        var data = { opponent: 'petya'};
-        socket.emit('get-opponent', data);
     });
 });
