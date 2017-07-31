7f77d78f3bbd96f14131e83f2681394fb5a42953
zanoooda <zanooda@ya.ru>
2017-07-31 13:44:22 +0300

    004

--- index.js
+++ index.js
@@ -13,14 +13,25 @@ io.on('connection', function (socket) {
 
     socket.on('play', function (data) {
         //
-        console.log('who want to play, looking for the opponent');
-        var data = { opponent: 'petya'};
+        console.log(socket.id.toString() + ' want to play, looking for the opponent');
+
+        socket.isOnline = true;
+        socket.isPlaying = false;
 
         io.clients((error, clients) => {
             if (error) throw error;
-            console.log(clients); // => [6em3d4TJP8Et9EMNAAAA, G5p55dHhGgUnLUctAAAB]
+            //console.log(clients); // => [6em3d4TJP8Et9EMNAAAA, G5p55dHhGgUnLUctAAAB]
+            clients.forEach(function(element) {
+                console.log(element.id, element.isOnline, element.isPlaying)
+            }, this);
         });
 
+        // io.clients((error, clients) => {
+        //     if (error) throw error;
+        //     console.log(clients); // => [6em3d4TJP8Et9EMNAAAA, G5p55dHhGgUnLUctAAAB]
+        // });
+        
+        var data = { opponent: 'petya'};
         socket.emit('get-opponent', data);
     });
 });
