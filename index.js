84277356f2b1b61e23c1926b849698a30d50d6fd
zanoooda <zanooda@ya.ru>
2017-07-31 13:45:08 +0300

    5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Paradox server 003\n");
+    res.end("Paradox server 004\n");
 });
 
 server.listen(process.env.PORT);
