1b24528dc87f1383c3413e3afefd0581d143d336
zanoooda <zanooda@ya.ru>
2017-08-18 22:17:03 +0300

    new server works

--- index.js
+++ index.js
@@ -1,51 +1,63 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Paradox server 0.0.21\n");
+    res.end(Object.keys(io.sockets.connected).length + ' users online')
 });
-
-server.listen(process.env.PORT);
+server.listen(process.env.PORT || 3000);
 var io = require('socket.io')(server);
 
 io.on('connection', function (socket) {
+    console.log(socket.id, 'connected');
 
-    console.log(socket.id.toString() + ' connected');
+    socket.on('play', function() {
+        console.log(socket.id, 'play');
 
-    socket.on('play', function (data) {
-        console.log(socket.id.toString() + ' want to play, looking for the opponent');
+        socket.emit('counter', Object.keys(io.sockets.connected).length);
 
-        socket.isOnline = true;
-        socket.isPlaying = false;
+        socket.opponentId = getOpponentId(socket, io);
+        if(socket.opponentId) {
+            console.log(socket.id, 'opponent-found');
 
-        //
-        var b = false;
-        var ids = Object.keys(io.sockets.connected);
-        ids.forEach(function(id) {
-            var s = io.sockets.connected[id];
-            // do something with socket here
-            if(!b && s.isOnline && !s.isPlaying && s.id != socket.id) {
-                s.isPlaying = true;
-                s.player = 2;
-                socket.player = 1;
-                socket.isPlaying = true;
-                s.opponentId = socket.id;
-                socket.opponentId = s.id;
+            io.sockets.connected[socket.opponentId].opponentId = socket.id;
 
-                b = true;
-
-                socket.emit('opponent-found', socket.player);
-                socket.broadcast.to(s.id).emit('opponent-found', s.player);
-            }
-            //console.log(s.id, s.isOnline, s.isPlaying);
-        });
-
-        if(!b) {
-            // no opponents right now
+            io.sockets.connected[socket.opponentId].emit('opponent-found', 1);
+            socket.emit('opponent-found', 2);
         }
-        
     });
 
-    socket.on('move', function (data) {
-        socket.broadcast.to(socket.opponentId).emit('opponent-move', data);
+    socket.on('move', function(state) {
+        console.log(socket.id, 'move');
+
+        if(socket.opponentId && io.sockets.connected[socket.opponentId]) {
+            socket.broadcast.to(socket.opponentId).emit('move', state);
+        }
+    });
+
+    socket.on('disconnect', function() {
+        console.log(socket.id, 'disconnect');
+
+        if(socket.opponentId && io.sockets.connected[socket.opponentId]) {
+            io.sockets.connected[socket.opponentId].disconnect();
+        }
     });
 });
+
+function getOpponentId(socket, io) {
+    var opponentId = false;
+
+    var b = false;
+    var ids = Object.keys(io.sockets.connected);
+    ids.forEach(function(id) {
+        var s = io.sockets.connected[id];
+        if(
+            !b && 
+            s.id != socket.id &&
+            s.opponentId !== undefined && 
+            !s.opponentId
+        ) {
+            opponentId = s.id;
+            b = true;
+        }
+    });
+
+    return opponentId;
+}
