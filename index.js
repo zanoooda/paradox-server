a7c329ca9601579a77d8776b11e03c2e9e0b67d5
zanooda <zanooda@ya.ru>
2020-11-16 02:59:56 +0200

    ;

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
-    res.end(Object.keys(io.sockets.connected).length + ' users online')
+    res.end(Object.keys(io.sockets.connected).length + ' users online');
 });
 server.listen(process.env.PORT || 3000);
 var io = require('socket.io')(server);
